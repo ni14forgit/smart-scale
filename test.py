991cf652b9a22950d6827363ae194a8840367bb9
Nishant <ni14@duke.edu>
2020-04-25 23:04:55 -0400

    mamp

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ r = requests.get('http://google.com')
 print r.status_code
 print r.headers
 print r.text[0:1000]
+
+url = 'http://localhost:8888/scale/'
+myobj = {'mass': 'somevalue'}
+x = requests.post(url, data = myobj)
+print x.text 
