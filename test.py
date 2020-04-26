65cbebe0cdcd0368050f62eaa8b98323acbd99b8
Nishant <ni14@duke.edu>
2020-04-25 23:17:34 -0400

    hi

--- test.py
+++ test.py
@@ -4,7 +4,27 @@ print r.status_code
 print r.headers
 print r.text[0:1000]
 
-url = 'http://localhost:8888/scale/'
-myobj = {'mass': 'somevalue'}
-x = requests.post(url, data = myobj)
-print x.text 
+URL = "http://maps.googleapis.com/maps/api/geocode/json"
+  
+# location given here 
+location = "delhi technological university"
+  
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = {'address':location} 
+  
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+  
+# extracting data in json format 
+data = r.json() 
+  
+  
+# extracting latitude, longitude and formatted address  
+# of the first matching location 
+latitude = data['results'][0]['geometry']['location']['lat'] 
+longitude = data['results'][0]['geometry']['location']['lng'] 
+formatted_address = data['results'][0]['formatted_address'] 
+  
+# printing the output 
+print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
+      %(latitude, longitude,formatted_address)) 
